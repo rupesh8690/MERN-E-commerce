f4b17a38cacec01195dc95e12a6039cd106f0b5f
rupesh8690 <rupeshlove8690@gmail.com>
2025-07-29 18:35:43 +0530

    completed authentication in backend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express"
+import authRoutes from "./routes/auth.route.js"
+
+import dotenv from "dotenv"
+dotenv.config()
+
+import cookieParser from "cookie-parser"
+
+
+import {connectDB} from "./lib/db.js"
+
+const app=express()
+const PORT=process.env.PORT
+
+app.use(express.json()); //The line app.use(express.json()) is used in Express.js to parse incoming JSON request bodies and make the data available in req.body.
+app.use(cookieParser())
+
+app.use("/api/auth",authRoutes);
+
+
+app.listen(PORT,()=>{
+    console.log("server is running on http://localhost:"+PORT)
+    //call connectDB
+    connectDB();
+})
